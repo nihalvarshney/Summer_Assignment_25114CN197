a231cdff3e25e7013a9c21277d53979fe08453b3
nihalvarshney <varshneynihal72@gmail.com>
2026-06-10 22:55:52 +0530

    this is my 10 commit

---
+++ Day_10/file2.c
@@ -0,0 +1,14 @@
+//reverse star pattern
+#include <stdio.h>
+int main() {
+    int i, j, space, rows = 5;
+    for(i = rows; i >= 1; i--) 
+    {
+        for(space = 1; space <= rows - i; space++)
+            printf(" ");
+        for(j = 1; j <= 2*i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
