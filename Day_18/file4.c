9662e19d9b4c1421128020dfa370170bbf7bef8e
nihalvarshney <varshneynihal72@gmail.com>
2026-06-18 22:04:04 +0530

    this is my 18 commit

---
+++ Day_18/file4.c
@@ -0,0 +1,15 @@
+//sort array in descending order
+#include <stdio.h>
+int main() {
+    int n,i,j;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++) scanf("%d",&arr[i]);
+    for(i=0;i<n-1;i++)
+        for(j=i+1;j<n;j++)
+            if(arr[i]<arr[j]) {
+                int temp=arr[i]; arr[i]=arr[j]; arr[j]=temp;
+            }
+    for(i=0;i<n;i++) printf("%d ",arr[i]);
+    return 0;
+}
